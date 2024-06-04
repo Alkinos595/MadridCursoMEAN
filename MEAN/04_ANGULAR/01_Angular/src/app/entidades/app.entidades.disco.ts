feb98d325153aac5203c3f2a8f645aa5e19e9e69
Alkinos595 <foinquinosalberto@gmail.com>
2024-06-04 09:35:45 +0200

    asdf

--- MEAN/04_ANGULAR/01_Angular/src/app/entidades/app.entidades.disco.ts
+++ MEAN/04_ANGULAR/01_Angular/src/app/entidades/app.entidades.disco.ts
@@ -1,17 +1,41 @@
 
 export class Disco {
 
-    public constructor(
-            public titulo : string|null = null, 
-            public grupo  : string|null = null,
-            public year   : number|null = null){
-        }
+	public titulo: string = '';
+	public grupo: string  = '';
+	public year: string   = '';
 
-    public vaciar():void{
-        this.titulo = null
-        this.grupo  = null
-        this.year   = null
-    }
-
+	public constructor(parametros: any) {
+		if (parametros.titulo) {
+			this.titulo = parametros.titulo
+		}
+		if (parametros.grupo) {
+			this.grupo = parametros.grupo
+		}
+		if (parametros.year) {
+			this.year = parametros.year
+		}
+	}
 }
 
+/*
+export class Disco {
+	constructor(
+		public titulo: string = '',
+		public grupo: string = '',
+		public year: string = ''
+	){}
+}
+
+export class Disco {
+	public titulo: string = '';
+	public grupo: string = '';
+	public year: string = '';
+
+	constructor(titulo: string, grupo: string, year: string) {
+		this.titulo = titulo;
+		this.grupo = grupo;
+		this.year = year;
+	}
+}
+*/
